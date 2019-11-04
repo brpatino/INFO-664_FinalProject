45e027e4363c184271893b6c767640df0f09fbca
brpatino <bpatino@pratt.edu>
2019-11-04 16:05:39 -0500

    third commit

--- finalproj.py
+++ finalproj.py
@@ -18,14 +18,80 @@
 
 import csv
 
-with open('PHdata.csv') as data_input:
-    data_results = csv.reader(data_input)
+with open('Caloocan_police.csv') as caloocan_police_data_input:
+    caloocan_police_results = csv.reader(caloocan_police_data_input)
     
-    next(data_results)
+    next(caloocan_police_results)
     
-    for row in data_results:
-        date = row[0]
-        location = row[1]
-        station = row[2]
-        type_killing = row[3]
+    for row in caloocan_police_results:
+        id_victim = row[0]
+        date = row[1]
+        location = row[2]
+        station = row[3]
+        type_killing = row[4]
+        print(date)
+        
+with open('Caloocan_unidentified.csv') as caloocan_unidentified_data_input:
+    caloocan_unidentified_results = csv.reader(caloocan_unidentified_data_input)
+    
+    next(caloocan_unidentified_results)
+    
+    for row in caloocan_unidentified_results:
+        id_victim = row[0]
+        date = row[1]
+        location = row[2]
+        station = row[3]
+        type_killing = row[4]
+        print(date)
+
+with open('QuezonCity_police.csv') as QuezonCity_police_data_input:
+    QuezonCity_police_results = csv.reader(QuezonCity_police_data_input)
+    
+    next(QuezonCity_police_results)
+    
+    for row in QuezonCity_police_results:
+        id_victim = row[0]
+        date = row[1]
+        location = row[2]
+        station = row[3]
+        type_killing = row[4]
+        print(date)
+        
+with open('QuezonCity_unidentified.csv') as QuezonCity_unidentified_data_input:
+    QuezonCity_unidentified_results = csv.reader(QuezonCity_unidentified_data_input)
+    
+    next(QuezonCity_unidentified_results)
+    
+    for row in QuezonCity_unidentified_results:
+        id_victim = row[0]
+        date = row[1]
+        location = row[2]
+        station = row[3]
+        type_killing = row[4]
+        print(date)
+        
+with open('Manila_police.csv') as Manila_police_data_input:
+    Manila_police_results = csv.reader(Manila_police_data_input)
+    
+    next(Manila_police_results)
+    
+    for row in Manila_police_results:
+        id_victim = row[0]
+        date = row[1]
+        location = row[2]
+        station = row[3]
+        type_killing = row[4]
+        print(date)
+        
+with open('Manila_unidentified.csv') as Manila_unidentified_data_input:
+    Manila_unidentified_results = csv.reader(Manila_unidentified_data_input)
+    
+    next(Manila_unidentified_results)
+    
+    for row in Manila_unidentified_results:
+        id_victim = row[0]
+        date = row[1]
+        location = row[2]
+        station = row[3]
+        type_killing = row[4]
         print(date)
